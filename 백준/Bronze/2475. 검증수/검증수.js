9215480518d46e770f65d667e3c3bee1fb2a5406
Choi Dongin <101073665+JaiIn@users.noreply.github.com>
2024-04-15 22:27:54 +0800

    [Bronze V] Title: 검증수, Time: 124 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.js
+++ 백준/Bronze/2475. 검증수/검증수.js
@@ -3,13 +3,7 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(filePath, "utf8").toString().split(" ").map(Number);
 
-let sum = 0;
-const sumFun = (arr, _sum) => {
-    for( let i = 0;i<arr.length;++i){
-        _sum += (arr[i] * arr[i]);
-    }
-    return _sum;
-}
+const result = input.reduce((sum, cur)=> sum + cur * cur,0) % 10;
 
 
-console.log(sumFun(input,sum)%10);
+console.log(result);
