4a92131563de435ad64a5ebf1170fc0a98cbdae8
Choi Dongin <101073665+JaiIn@users.noreply.github.com>
2024-04-22 18:14:22 +0800

    [Bronze II] Title: 단어의 개수, Time: 204 ms, Memory: 17348 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.js
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -1,13 +1,12 @@
 const fs = require("fs");
 
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-const input = fs.readFileSync(filePath, "utf8").toString()
+const input = fs.readFileSync(filePath, "utf8").toString().trim().split(" ");
 
-let input_arr = input.trim().split(" ");
 let count = 0;
 
-for (let i = 0; i < input_arr.length; ++i) {
-    if (input_arr[i] != '')
+for (let i = 0; i < input.length; ++i) {
+    if (input[i] != '')
         ++count;
 }
 
