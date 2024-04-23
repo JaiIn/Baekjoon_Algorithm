0c6bd8d707f9e2241638d2321603bded15288a6c
Choi Dongin <101073665+JaiIn@users.noreply.github.com>
2024-04-23 18:45:06 +0800

    [Bronze V] Title: Hello World, Time: 112 ms, Memory: 9320 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs.readFileSync(filePath, "utf8").toString().trim().split(' ').map(Number);
+
+console.log("Hello World!");
