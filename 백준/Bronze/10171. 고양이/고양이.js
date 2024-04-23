6925227194b9595a87fd98a7594c54ed6fac789a
Choi Dongin <101073665+JaiIn@users.noreply.github.com>
2024-04-23 19:27:29 +0800

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 9320 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs.readFileSync(filePath, "utf8").toString().trim().split(' ').map(Number);
+
+console.log(`\\    /\\
+ )  ( ')
+(  /  )
+ \\(__)|`)
