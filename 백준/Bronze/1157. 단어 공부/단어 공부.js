617e9c02edf968d33ce29cdf0150ad5bd867c0d6
Choi Dongin <101073665+JaiIn@users.noreply.github.com>
2024-05-06 16:57:10 +0800

    [Bronze I] Title: 단어 공부, Time: 228 ms, Memory: 35060 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.js
+++ 백준/Bronze/1157. 단어 공부/단어 공부.js
@@ -3,37 +3,16 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(filePath, "utf8").toString().trim().split('\n');
 
-const al = new Array(26).fill(0);
+let arr = input.join('').toUpperCase();
 
-let arr = input.map(a => a.toUpperCase().split("")).flat();
-
-const mostUsed = (_arr) => {
-    for (let i = 0; i < _arr.length; ++i) {
-        if (_arr[i]) {
-            ++al[_arr[i].charCodeAt() - 65];
-        }
-    }
-    
-    let most = -1;
-    let mostIdx = -1;
-    let shuangmost = -1;
-    
-    for (let i = 0; i < al.length; ++i) {
-        if(most < al[i]){
-            most = al[i];
-            mostIdx = i;
-            shuangmost = -1;
-        }
-        else if(most === al[i]){
-            shuangmost = al[i]
-        }
-    }
-    
-    if(shuangmost !== -1 && most === shuangmost){
+const mostUsed = (arr) => {
+    const counts = new Array(26).fill(0);
+    arr.split('').forEach(char=>counts[char.charCodeAt() - 65]++);
+    const maxCount = Math.max(...counts);
+    if(counts.indexOf(maxCount) !== counts.lastIndexOf(maxCount)) {
         return "?";
-    }
-    else{
-        return String.fromCharCode(mostIdx+65);
+    } else{
+        return String.fromCharCode(counts.indexOf(maxCount)+65);
     }
 }
 
